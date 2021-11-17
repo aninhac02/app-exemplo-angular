6156d705e86a22a5617ce1649f4e549dd11540fe
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-17 16:54:01 -0300

    adicionando as rotas

--- src/app/Model/Pessoa/listar/listar.component.ts
+++ src/app/Model/Pessoa/listar/listar.component.ts
@@ -1,14 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-listar',
   templateUrl: './listar.component.html'
 })
-export class ListarComponent implements OnInit {
+export class ListarComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  
+constructor(){}
+ 
 }
