b982a7f6992b4f3b2e3eaa7f6fdf9bf6829ad3c9
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-18 13:51:50 -0300

    criacao do service

---
+++ src/app/Service/servico.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pessoa } from '../Model/Pessoa/entidade/Pessoa';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ServicoService {
+
+  constructor(private http: HttpClient) {
+
+   }
+
+   urlBase = "http://localhost:8080/pessoa";
+
+   getPessoa(){
+     return this.http.get<Pessoa[]>(this.urlBase);
+   }
+}
