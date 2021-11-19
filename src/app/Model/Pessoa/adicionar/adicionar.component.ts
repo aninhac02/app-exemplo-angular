3ad225aa2ef1c444a2f4ac20a6c0a9178c40dddf
Ana Caroline <anacarolinevilela0@gmail.com>
2021-11-18 21:22:43 -0300

    metodo de adicionar

--- src/app/Model/Pessoa/adicionar/adicionar.component.ts
+++ src/app/Model/Pessoa/adicionar/adicionar.component.ts
@@ -1,14 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicoService } from 'src/app/Service/servico.service';
+import { Pessoa } from '../entidade/Pessoa';
 
 @Component({
   selector: 'app-adicionar',
   templateUrl: './adicionar.component.html'
 })
 export class AdicionarComponent implements OnInit {
-
-  constructor() { }
+  
+  pessoa: Pessoa = new Pessoa();
+  constructor(private service: ServicoService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  addPessoa(){
+    this.service.addPessoa(this.pessoa)
+    .subscribe(data => {
+      alert('Cadastrado com sucesso');
+      this.router.navigate(['listar']); 
+    })
   }
 
 }
